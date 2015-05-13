3b9527fd9be26121389f10c05cdf83cf88f29ca0
DerekTBrown <derek@allderek.com>
2015-05-13 15:27:11 -0400

    Inital commit of sudoslash files.

--- templates/header.php
+++ templates/header.php
@@ -1,21 +1,7 @@
-<?php  
+<?php
   $class = 'no-cover';
   if ( is_front_page() || is_archive() || is_search() || is_home() ) {
     $class = 'cover';
-  }
-
-  if ( is_single() ) {
-    $post_thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
-    if ( ! empty($post_thumbnail_url) ) {
-      $class = 'cover';
-      ?>
-      <style>
-        .banner.cover {
-          background-image: url( <?php echo $post_thumbnail_url; ?> );
-        }
-      </style>
-      <?php
-    }
   }
 ?>
 
@@ -23,6 +9,9 @@
   <div class="header-inner">
       <nav class="nav-main" role="navigation">
         <div class="container">
+          <div class="no-cover-logo" style="text-align: left;">
+            <img src="/wp-content/themes/sudoslash/assets/img/logo_pu.png" />
+          </div>
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
@@ -33,10 +22,9 @@
       <?php if( is_front_page() || is_archive() || is_home() ) : ?>
         <hgroup>
           <div class="container">
-            <h1 class="page-title">
-              <?php echo dw_timeline_title(); ?>
-            </h1>
-            <h2 class="page-description"><?php bloginfo('description'); ?></h2>
+            <div class="page-title">
+              <img src="/wp-content/themes/sudoslash/assets/img/logo.png" />
+            </div>
             <button id="get-started" class="btn btn-default btn-coner"><?php echo dw_timeline_get_theme_option('get_start','Get Start Now') ?></button>
           </div>
         </hgroup>
